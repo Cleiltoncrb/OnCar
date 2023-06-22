31e4fe375ffb874dab873a7b75f1053f048bf333
Cleiltoncrb <cleilton.oi18@gmail.com>
2023-06-22 13:02:53 -0300

    Projeto (JAVA - Ofica)

---
+++ src/main/java/br/com/oncar/OncarApplication.java
@@ -0,0 +1,13 @@
+package br.com.oncar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OncarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OncarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/oncar/OncarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.oncar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OncarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
